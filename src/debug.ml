3def6212991ce1e048d4fed71257dcb4b4a3d3d2
Simon Marc <marc.simon42@gmail.com>
2013-06-22 17:18:57 -0700

    [fix css]

--- src/debug.ml
+++ src/debug.ml
@@ -4,7 +4,7 @@
   open Html5
   open D
 
-  let box_dom = div ~a:[ a_style "position:fixed;top:0;right:0;width:200px;background-color:rgba(0,0,0,0.3);padding:0 10px" ] []
+  let box_dom = div ~a:[ a_style "position:fixed;top:0;right:0;width:200px;background-color:rgba(0,0,0,0.3);padding:0 10px;max-height:500px; overflow:scroll" ] []
 
   let reset_btn = button ~button_type:`Button [ pcdata "reset" ]
 
